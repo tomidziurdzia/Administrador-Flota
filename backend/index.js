075cbda5bc160fa54e05c0ef19695c329f066900
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-19 18:47:12 -0300

    Conecte la Base de Datos de MONGO.DB a la App. Despues instale dotenv y guarde todo en variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 const app = express();
 
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
 app.listen(4000, () => {
-  console.log("Servidor corriendo en el puerto 4000");
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
