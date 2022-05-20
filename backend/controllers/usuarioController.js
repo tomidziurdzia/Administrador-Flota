c53d75a9b05c0c9cce69623efd406039b302dacb
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-20 16:04:12 -0300

    Creo una funcion autenticar y dentro tengo que revisar si el usuario existe. Si existe tengo que revisar si el usuario esta confirmado

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,8 @@
 import Usuario from "../models/Usuario.js";
 import generarId from "../helpers/generarId.js";
+import generarJWT from "../helpers/generarJWT.js";
 
+// Registrar Usuario
 const registrar = async (req, res) => {
   // Evitar registros duplicados
   const { email } = req.body;
@@ -17,6 +19,24 @@ const registrar = async (req, res) => {
     res.json(usuarioAlmacenado);
   } catch (error) {
     console.log(error);
+  }
+};
+
+// Autenticar Usuario
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  // Comprobar si el Usuario esta o no confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmada");
+    return res.status(404).json({ msg: error.message });
   }
 };
 
