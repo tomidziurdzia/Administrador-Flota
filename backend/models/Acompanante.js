208069c541134ce93302ebe909aff94af45ec1e8
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-21 18:46:19 -0300

    Creo el modelo Acompañante

---
+++ backend/models/Acompanante.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const acompanantesSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    apellido: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    cuil: {
+      type: Number,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    creador: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+    },
+    colaboradores: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Usuario",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Acompanante = mongoose.model("Acompanante", acompanantesSchema);
+export default Acompanante;
