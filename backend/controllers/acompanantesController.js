ff9cdf27cd3992217c901322587a354039fafa50
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-21 18:49:02 -0300

    Creo el Controlador de Acompañantes

---
+++ backend/controllers/acompanantesController.js
@@ -0,0 +1,23 @@
+const obtenerAcompanantes = async (req, res) => {};
+
+const nuevoAcompanante = async (req, res) => {};
+
+const obtenerAcompanante = async (req, res) => {};
+
+const actualizarAcompanante = async (req, res) => {};
+
+const eliminarAcompanante = async (req, res) => {};
+
+const agregarColaborador = async (req, res) => {};
+
+const eliminarColaborador = async (req, res) => {};
+
+export {
+  obtenerAcompanantes,
+  nuevoAcompanante,
+  obtenerAcompanante,
+  actualizarAcompanante,
+  eliminarAcompanante,
+  agregarColaborador,
+  eliminarColaborador,
+};
