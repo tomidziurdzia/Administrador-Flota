f2ec590646a98a83496aa26f298fde33687671c1
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-21 17:31:00 -0300

    Obtener Chofer funcion

--- backend/controllers/choferesController.js
+++ backend/controllers/choferesController.js
@@ -4,6 +4,7 @@ const obtenerChoferes = async (req, res) => {
   const choferes = await Chofer.find().where("creador").equals(req.usuario);
   res.json(choferes);
 };
+
 const nuevoChofer = async (req, res) => {
   const chofer = new Chofer(req.body);
   chofer.creador = req.usuario._id;
@@ -15,7 +16,25 @@ const nuevoChofer = async (req, res) => {
     console.log(error);
   }
 };
-const obtenerChofer = async (req, res) => {};
+
+const obtenerChofer = async (req, res) => {
+  const { id } = req.params;
+
+  const chofer = await Chofer.findById(id);
+
+  if (!chofer) {
+    const error = new Error("Chofer no encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (chofer.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  res.json(chofer);
+};
+
 const actualizarChofer = async (req, res) => {};
 const eliminarChofer = async (req, res) => {};
 const agregarColaborador = async (req, res) => {};
