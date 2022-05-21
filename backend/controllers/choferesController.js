314d89382e6cc15176c91e0488956f6b59df1166
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-21 18:11:54 -0300

    Funcion de Actualizar Datos Chofer

--- backend/controllers/choferesController.js
+++ backend/controllers/choferesController.js
@@ -35,7 +35,38 @@ const obtenerChofer = async (req, res) => {
   res.json(chofer);
 };
 
-const actualizarChofer = async (req, res) => {};
+const actualizarChofer = async (req, res) => {
+  const { id } = req.params;
+  const chofer = await Chofer.findById(id);
+
+  if (!chofer) {
+    const error = new Error("El chofer no fue encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (chofer.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  chofer.nombre = req.body.nombre || chofer.nombre;
+  chofer.apellido = req.body.apellido || chofer.apellido;
+  chofer.cuil = req.body.cuil || chofer.cuil;
+  chofer.fechaCarnet = req.body.fechaCarnet || chofer.fechaCarnet;
+  chofer.fechaLinti = req.body.fechaLinti || chofer.fechaLinti;
+  chofer.fechaPsicofisico =
+    req.body.fechaPsicofisico || chofer.fechaPsicofisico;
+  chofer.fechaLibretaSanitaria =
+    req.body.fechaLibretaSanitaria || chofer.fechaLibretaSanitaria;
+
+  try {
+    const choferAlmacenado = await chofer.save();
+    res.json(choferAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const eliminarChofer = async (req, res) => {};
 const agregarColaborador = async (req, res) => {};
 const eliminarColaborador = async (req, res) => {};
