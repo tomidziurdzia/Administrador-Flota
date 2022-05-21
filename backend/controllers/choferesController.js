76d25c4c9d05762877c4f4ff15bee79dd1d08635
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-21 16:47:09 -0300

    Creo el Controlador de Choferes

---
+++ backend/controllers/choferesController.js
@@ -0,0 +1,31 @@
+const obtenerChoferes = async (req, res) = {
+
+}
+const nuevoChofer = async (req, res) = {
+  
+}
+const obtenerChofer = async (req, res) = {
+  
+}
+const actualizarChofer = async (req, res) = {
+  
+}
+const eliminarChofer = async (req, res) = {
+  
+}
+const agregarColaborador = async (req, res) = {
+  
+}
+const eliminarColaborador = async (req, res) = {
+  
+}
+
+export {
+  obtenerChoferes,
+  nuevoChofer,
+  obtenerChofer,
+  actualizarChofer,
+  eliminarChofer,
+  agregarColaborador,
+  eliminarColaborador
+}
