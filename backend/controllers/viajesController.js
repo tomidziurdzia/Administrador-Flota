963edf26090a3836bbc259d057d4797d8d0867c8
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-22 18:42:45 -0300

    Funcion Actualizar Viaje

--- backend/controllers/viajesController.js
+++ backend/controllers/viajesController.js
@@ -56,7 +56,33 @@ const obtenerViaje = async (req, res) => {
   res.json(viaje);
 };
 
-const actualizarViaje = async (req, res) => {};
+const actualizarViaje = async (req, res) => {
+  const { id } = req.params;
+
+  const viaje = await Viaje.findById(id);
+  if (!viaje) {
+    const error = new Error("El viaje no fue encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (viaje.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  viaje.fecha = req.body.fecha || viaje.fecha;
+  viaje.patente = req.body.patente || viaje.patente;
+  viaje.chofer = req.body.chofer || viaje.chofer;
+  viaje.acompanante = req.body.acompanante || viaje.acompanante;
+  viaje.destino = req.body.destino || viaje.destino;
+
+  try {
+    const viajeAlmacenado = await viaje.save();
+    res.json(viajeAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarViaje = async (req, res) => {};
 
