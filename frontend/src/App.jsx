c0ce467fc0938bbfb05347c850205822772e871e
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-23 16:42:45 -0300

    Cree las paginas, las importe a App e hice todo el routing. Use Outlet en AuthLayout ya que son rutas anidadas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
+import Login from "./paginas/Login";
+import Registrar from "./paginas/Registrar";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
+import OlvidePassword from "./paginas/OlvidePassword";
+import NuevoPassword from "./paginas/NuevoPassword";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthLayout />}></Route>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="olvide-password/:token" element={<NuevoPassword />} />
+          <Route path="confirmar" element={<ConfirmarCuenta />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  return (
+    <>
+      <div>AuthLayout</div>
+      <Outlet />
+    </>
+  );
 };
 
 export default AuthLayout;
