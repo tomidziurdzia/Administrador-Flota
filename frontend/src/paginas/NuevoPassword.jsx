559bc5def4c0e4566ef04f93e926b3962deba20c
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-24 17:12:40 -0300

    Validando Token para resetear Password y Almacenando nueva Password

--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,57 +1,127 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import axios from "axios";
 
 const NuevoPassword = () => {
-  <>
-    <h1 className="text-primary-blue font-black text-center text-2xl md:text-4xl">
-      Reestablecer Contraseña
-    </h1>
-    <form className="my-5 bg-white shadow rounded-lg p-10">
-      <div className="mt-8 my-5">
-        <label
-          className="text-gray-500 block text-xl font-bold"
-          htmlFor="contraseña"
+  const [password, setPassword] = useState("");
+  const [repetirPassword, setRepetirPassword] = useState("");
+  const [passwordModificado, setPasswordModificado] = useState(false);
+
+  const [tokenValido, setTokenValido] = useState(false);
+  const [alerta, setAlerta] = useState({});
+  const params = useParams();
+  const { token } = params;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        //TODO : mover a cliente axios
+        await axios(
+          `http://localhost:4000/api/usuarios/olvide-password/${token}`
+        );
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+
+    comprobarToken();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== repetirPassword) {
+      setAlerta({
+        msg: "Las contraseñas no coinciden",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      // TODO: mover hacia un cliente axios
+      const url = `http://localhost:4000/api/usuarios/olvide-password/${token}`;
+      const { data } = await axios.post(url, { password });
+
+      setAlerta({ msg: data.msg, error: false });
+      setPassword("");
+      setRepetirPassword("");
+      setPasswordModificado(true);
+
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
+    } catch (error) {
+      setAlerta({ msg: error.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alerta;
+
+  return (
+    <>
+      <h1 className="text-primary-blue font-black text-center text-2xl md:text-4xl">
+        Reestablecer Contraseña
+      </h1>
+      {msg && <Alerta alerta={alerta} />}
+      {tokenValido && (
+        <form
+          className="my-5 bg-white shadow rounded-lg p-10"
+          onSubmit={handleSubmit}
         >
-          Contraseña
-        </label>
-        <input
-          className="w-full mt-2 p-3 border rounded-xl"
-          id="contraseña"
-          type="password"
-          placeholder="Ingresa tu contraseña"
-        />
-      </div>
-      <div className="mt-8 my-5">
-        <label
-          className="text-gray-500 block text-xl font-bold"
-          htmlFor="repetir-contraseña"
-        >
-          Repetir Contraseña
-        </label>
-        <input
-          className="w-full mt-2 p-3 border rounded-xl"
-          id="repetir-contraseña"
-          type="password"
-          placeholder="Repite tu contraseña"
-        />
-      </div>
-      <input
-        type="submit"
-        value="Reestablecer Contraseña"
-        className="bg-primary-blue text-white w-full py-3 font-bold rounded hover:cursor-pointer hover:bg-hover-button transition-colors"
-      />
-    </form>
-    <nav className="lg:flex lg:justify-between">
-      <Link className="block text-center my-5 text-gray-500 text-sm" to="/">
-        ¿Ya tienes una cuenta? Inicia sesión
-      </Link>
-      <Link
-        className="block text-center my-5 text-gray-500 text-sm"
-        to="/olvide-password"
-      >
-        Olvide mi contraseña
-      </Link>
-    </nav>
-  </>;
+          <div className="mt-8 my-5">
+            <label
+              className="text-gray-500 block text-xl font-bold"
+              htmlFor="contraseña"
+            >
+              Contraseña
+            </label>
+            <input
+              className="w-full mt-2 p-3 border rounded-xl"
+              id="contraseña"
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mt-8 my-5">
+            <label
+              className="text-gray-500 block text-xl font-bold"
+              htmlFor="repetir-contraseña"
+            >
+              Repetir Contraseña
+            </label>
+            <input
+              className="w-full mt-2 p-3 border rounded-xl"
+              id="repetir-contraseña"
+              type="password"
+              placeholder="Repite tu contraseña"
+              value={repetirPassword}
+              onChange={(e) => setRepetirPassword(e.target.value)}
+            />
+          </div>
+          <input
+            type="submit"
+            value="Reestablecer Contraseña"
+            className="bg-primary-blue text-white w-full py-3 font-bold rounded hover:cursor-pointer hover:bg-hover-button transition-colors"
+          />
+        </form>
+      )}
+      {passwordModificado && (
+        <Link className="block text-center my-5 text-gray-500 text-sm" to="/">
+          ¿Ya tienes una cuenta? Inicia sesión
+        </Link>
+      )}
+    </>
+  );
 };
 
 export default NuevoPassword;
