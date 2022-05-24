9ba35df8232e84e807b431b05cba2e4644a7bd69
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-24 18:05:24 -0300

    Redireccionando al Usuario si esta Autenticado

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect, createContext } from "react";
 import clienteAxios from "../config/clienteAxios";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+  const [cargando, setCargando] = useState(true);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const autenticarUsuario = async () => {
       const token = localStorage.getItem("token");
 
       if (!token) {
+        setCargando(false);
         return;
       }
 
@@ -23,16 +28,21 @@ const AuthProvider = ({ children }) => {
 
       try {
         const { data } = await clienteAxios("/usuarios/perfil", config);
+        setAuth(data);
+
+        //Esta sirve para no iniciar sesion a cada rato. Me guarda la sesion
+        navigate("/dashboard");
       } catch (error) {
-        console.log(error);
+        setAuth({});
       }
+      setCargando(false);
     };
 
     autenticarUsuario();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, cargando }}>
       {children}
     </AuthContext.Provider>
   );
--- frontend/src/layouts/RutaProtegida.jsx
+++ frontend/src/layouts/RutaProtegida.jsx
@@ -2,7 +2,9 @@ import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 const RutaProtegida = () => {
-  const { auth } = useAuth();
+  const { auth, cargando } = useAuth();
+
+  if (cargando) return "Cargando...";
 
   return <>{auth._id ? "Autenticado" : <Navigate to="/" />}</>;
 };
